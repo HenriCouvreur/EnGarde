fcfb6a539f055f9f6eae78071c43955f81b3f132
Henri Couvreur <h.couvreur@rubika-edu.com>
2017-05-07 01:10:31 +0200

    More testing

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,23 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class Health : MonoBehaviour 
+public class Health : NetworkBehaviour  //that only works if the gameObject has a NetworkIdentity component
 {
 	public  const int maxHealth = 100;
-	public int currentHealth = maxHealth;
+
+	//SyncVar = this variable only exists on the server, and is updated on all clients at all time
+	//Hook = the function to call when the syncvar changes -> if health changes, change healthbar on all clients.
+	//The hook is called right before the value changes. It takes the new value as a parameter.
+	[SyncVar (hook = "OnChangeHealth")] public int currentHealth = maxHealth;
+
 	public RectTransform healthbar;
+	public bool destroyOnDeath;
+	private NetworkStartPosition[] spawnPoints;
+
+	void Start()
+	{
+		if (isLocalPlayer)
+			spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+	}
 
 	public void TakeDamage(int amount)
 	{
+		//only the server can access this function
+		if (!isServer)
+			return;
+
 		currentHealth -= amount;
 
 		if (currentHealth <= 0)
 		{
-			currentHealth = 0;
-			Debug.Log("Dead");
+			if (destroyOnDeath)
+				Destroy(gameObject);
+			
+			else
+			{
+				currentHealth = maxHealth;
+				RpcSpawn();
+			}
 		}
+	}
 
-		healthbar.sizeDelta = new Vector2(currentHealth*2, healthbar.sizeDelta.y);
+	void OnChangeHealth (int health)
+	{
+		healthbar.sizeDelta = new Vector2(health * 2, healthbar.sizeDelta.y);
+	}
+
+	[ClientRpc]
+	void RpcSpawn () //has to start with "Rpc"
+	{
+		if (isLocalPlayer) //has to be initiated by the client, as NetworkTranform overrides server instructions
+		{
+			Vector3 spawnPoint = Vector3.zero;
+			if (spawnPoints != null & spawnPoints.Length > 0)
+				spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+			transform.position = spawnPoint;
+		}
 	}
 }
